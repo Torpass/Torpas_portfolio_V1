9227726218d965e2d3835f0c30d1b7f3cd9d4c7b
Pastor José Jiménez Lucena <pastor.jimenez.0806@gmail.com>
2024-03-27 20:22:04 -0400

    href added into ecomoda project

--- app/page.js
+++ app/page.js
@@ -228,14 +228,15 @@ function Homepage() {
             </div>
             <div className="w-full h-[0.4px] dark:bg-neutral-600 bg-neutral-400/60 mt-1 top-7  " />
           </div>
-
-          <Image
-            width={900}
-            height={300}
-            className="h-full duration-300 hover:scale-110 transition-all ease-in-out object-cover"
-            src={"/Ecomoda.jpeg"}
-            alt="Ecomoda project"
-          />
+          <a target="_blank" href="https://github.com/Torpass/ECOMODA_FINANCIAL_API">
+            <Image
+              width={900}
+              height={300}
+              className="h-full duration-300 hover:scale-110 transition-all ease-in-out object-cover"
+              src={"/Ecomoda.jpeg"}
+              alt="Ecomoda project"
+            />
+          </a>
         </div>
         <div className="dark:bg-[#1E1E1E] bg-white border dark:border-neutral-600 border-neutral-400/60 shadow-xl rounded-lg col-span-2  relative  overflow-hidden  ">
           <div className=" absolute w-full p-2 z-20">
